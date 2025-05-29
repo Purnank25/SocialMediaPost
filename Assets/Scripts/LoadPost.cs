f67139e84d60672969df8ce1cae34a4d5ad5a3e5
Purnank25 <purnankexcel1@gmail.com>
2025-05-29 22:49:14 +0530

    added class PostData

---
+++ Assets/Scripts/LoadPost.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PostData
+{
+    public string userName;
+    public string postContent;
+    public int likeCount;
+    public string profilePicture; 
+}
+public class PostList
+{
+    public PostData[] posts;
+}
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public Image profilePicture;
+
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+}
