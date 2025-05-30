ab428461c44c0de229662611fd07fdfa766756d6
Purnank25 <purnankexcel1@gmail.com>
2025-05-30 16:36:41 +0530

    single post retrived

--- Assets/Scripts/LoadPost.cs
+++ Assets/Scripts/LoadPost.cs
@@ -1,46 +1,26 @@
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
+using TMPro;
 
 public class LoadPost : MonoBehaviour
 {
-    public TextMeshProUGUI username;
-    public TextMeshProUGUI postContent;
-    public Image profilePicture;
+    public TextMeshProUGUI usernameText;
+    public Image profilePic;
+    public TextMeshProUGUI contentText;
     public Button likeButton;
     public TextMeshProUGUI likeCountText;
-    public Image buttonImage;
-    public Sprite likedSprite;
-    public Sprite unlikedSprite;
+    public Sprite likedSprite, unlikedSprite;
 
-    private int likeCount = 0;
     private bool isLiked = false;
+    private int likeCount;
 
-    public void Load_Post(PostData data)
-    {
-        username.text = data.userName;
-        postContent.text = data.postContent;
-        likeCount = data.likeCount;
-        likeCountText.text = likeCount.ToString();
+   
 
-        Sprite pic = Resources.Load<Sprite>(data.profilePic);
-        if (pic != null)
-            profilePicture.sprite = pic;
-
-        UpdateLikeUI();
-        likeButton.onClick.AddListener(ToggleLike);
-    }
-
-    private void ToggleLike()
+    public void ToggleLike()
     {
         isLiked = !isLiked;
         likeCount += isLiked ? 1 : -1;
-        UpdateLikeUI();
-    }
-
-    private void UpdateLikeUI()
-    {
         likeCountText.text = likeCount.ToString();
-        buttonImage.sprite = isLiked ? likedSprite : unlikedSprite;
+        likeButton.image.sprite = isLiked ? likedSprite : unlikedSprite;
     }
 }
